1133592d5321c10659e29c03dc70b9a95c84725d
edmundrotimi <edmundrotimi@gmail.com>
2024-09-17 03:44:57 +0200

    custom user model

--- core/users/models.py
+++ core/users/models.py
@@ -1,3 +1,61 @@
-from django.db import models
+from datetime import datetime
 
-# Create your models here.
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+from simple_history.models import HistoricalRecords
+
+
+# set default date_time
+def set_date_time():
+    now = timezone.now()
+    return timezone.make_aware(datetime(now.year, 11, 30, 23, 59))
+
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('Users require an email field')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Users require an email field')
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
+
+
+class User(AbstractUser):
+    id = models.AutoField(primary_key=True, editable=False)  # noqa: A003
+    username = None
+    email = models.EmailField(_('email address'), unique=True)
+    history = HistoricalRecords()
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS: list[str] = []
+
+    def __str__(self):
+        return self.email
