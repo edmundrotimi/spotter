894517d1b1cd88d33b3549e0a596235d68df5718
edmundrotimi <edmundrotimi@gmail.com>
2024-09-18 20:03:08 +0200

    update book app models and admin

--- core/books/admin.py
+++ core/books/admin.py
@@ -1,3 +1,125 @@
-# from django.contrib import admin
+from django.contrib import admin
+from unfold.admin import ModelAdmin
 
-# Register your models here.
+from .models import Author, Book
+
+
+class AuthorAdmin(ModelAdmin):
+    list_display = ['id', 'name', 'gender', 'ratings_count', 'average_rating', 'text_reviews_count']
+    list_display_links = ['id', 'ratings_count', 'average_rating', 'text_reviews_count']
+    search_fields = ['id', 'name']
+    list_per_page = 10
+    show_full_result_count = True
+    actions_on_top = True
+    actions_on_bottom = True
+    save_as = True
+    save_as_continue = True
+    save_on_top = True
+    fieldsets = [
+        [
+            'Bio',
+            {
+                'classes': ['wide', 'extrapretty'],
+                'fields': ['id', 'name', 'gender', 'image_url', 'about'],
+            },
+        ],
+        [
+            'Ratings',
+            {
+                'classes': ['collapse', 'wide', 'extrapretty'],
+                'fields': ['ratings_count', 'average_rating', 'text_reviews_count', 'fans_count'],
+            },
+        ],
+        [
+            'Works',
+            {
+                'classes': ['collapse', 'wide', 'extrapretty'],
+                'fields': ['work_ids', 'works_count'],
+            },
+        ],
+        [
+            'Books',
+            {
+                'classes': ['collapse', 'wide', 'extrapretty'],
+                'fields': ['book_ids'],
+            },
+        ],
+    ]
+
+
+class BookAdmin(ModelAdmin):
+    list_display = ['id', 'title', 'isbn']
+    list_display_links = ['id', 'title', 'isbn']
+    search_fields = ['id', 'title']
+    list_per_page = 100
+    show_full_result_count = True
+    actions_on_top = True
+    actions_on_bottom = True
+    save_as = True
+    save_as_continue = True
+    save_on_top = True
+    fieldsets = [
+        [
+            'General Information',
+            {
+                'classes': ['wide', 'extrapretty'],
+                'fields': ['id', 'title', 'isbn'],
+            },
+        ],
+        [
+            'Book Description',
+            {
+                'classes': ['wide', 'extrapretty'],
+                'fields': ['num_pages', 'language', 'image_url', 'shelves', 'description'],
+            },
+        ],
+        [
+            'Author and Co-Author',
+            {
+                'classes': ['collapse', 'wide', 'extrapretty'],
+                'fields': ['authors'],
+            },
+        ],
+        [
+            'Works',
+            {
+                'classes': ['collapse', 'wide', 'extrapretty'],
+                'fields': ['work_ids'],
+            },
+        ],
+        [
+            'Book Number',
+            {
+                'classes': ['collapse', 'wide', 'extrapretty'],
+                'fields': ['isbn13', 'asin'],
+            },
+        ],
+        [
+            'Ratings',
+            {
+                'classes': ['collapse', 'wide', 'extrapretty'],
+                'fields': ['average_rating', 'rating_dist', 'ratings_count', 'text_reviews_count'],
+            },
+        ],
+        [
+            'Publication Info',
+            {
+                'classes': ['collapse', 'wide', 'extrapretty'],
+                'fields': [
+                    'format', 'publication_date', 'original_publication_date', 'edition_information', 'publisher'
+                ],
+            },
+        ],
+        [
+            'Series',
+            {
+                'classes': ['collapse', 'wide', 'extrapretty'],
+                'fields': ['series_id', 'series_name', 'series_position'],
+            },
+        ],
+    ]
+
+
+# register admin settings
+admin.site.register(Book, BookAdmin)
+admin.site.register(Author, AuthorAdmin)
--- core/books/models.py
+++ core/books/models.py
@@ -1,3 +1,69 @@
-# from django.db import models
+from django.db import models
+from shortuuid.django_fields import ShortUUIDField
 
-# Create your models here.
+from core.works.models import Work
+
+
+class Author(models.Model):
+    id = ShortUUIDField(length=30, max_length=40, alphabet='16777216', primary_key=True)  # noqa: A003
+    name = models.CharField(max_length=100)
+    gender = models.CharField(max_length=50, null=True, blank=True)
+    image_url = models.CharField(max_length=200, null=True, blank=True)
+    about = models.TextField(max_length=2000, null=True, blank=True)
+    fans_count = models.IntegerField()
+    ratings_count = models.IntegerField()
+    average_rating = models.IntegerField()
+    role = models.CharField(max_length=300, null=True, blank=True)
+    text_reviews_count = models.IntegerField()
+    book_ids = models.ManyToManyField('Book')
+    work_ids = models.ManyToManyField(Work)
+    works_count = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.id}'
+
+    class Meta:
+        verbose_name = 'Author'
+        verbose_name_plural = 'Authors'
+
+
+class Book(models.Model):
+    id = ShortUUIDField(length=30, max_length=40, alphabet='123876', primary_key=True)  # noqa: A003
+    title = models.CharField(max_length=300)
+    author_id = models.ManyToManyField('Author')
+    work_id = models.ManyToManyField(Work)
+    isbn = models.CharField(max_length=200)
+    isbn13 = models.CharField(max_length=200)
+    asin = models.CharField(max_length=200)
+    language = models.CharField(max_length=50)
+    average_rating = models.FloatField(max_length=50)
+    rating_dist = models.CharField(max_length=300)
+    ratings_count = models.IntegerField()
+    text_reviews_count = models.IntegerField()
+    publication_date = models.CharField(max_length=100)
+    original_publication_date = models.CharField(max_length=100)
+    format = models.CharField(max_length=100)  # noqa: A003
+    edition_information = models.CharField(max_length=100)
+    image_url = models.CharField(max_length=100)
+    publisher = models.CharField(max_length=100)
+    num_pages = models.IntegerField()
+    series_id = models.CharField(max_length=100)
+    series_name = models.CharField(max_length=300)
+    series_position = models.CharField(max_length=100)
+    description = models.TextField(max_length=4000)
+
+    def __str__(self):
+        return f'{self.id}'
+
+
+class BookFav(models.Model):
+    id = ShortUUIDField(length=30, max_length=40, alphabet='12ge76', primary_key=True)  # noqa: A003
+    book = models.ManyToManyField('Book')
+    fav_count = models.IntegerField(help_text='total number of fav for a book')
+
+    def __str__(self):
+        return f'{self.id}'
+
+    class Meta:
+        verbose_name = 'Book Fav'
+        verbose_name_plural = 'Book Fav'
