db9f76e1c0759f39e92fc6b836989124457c75a7
edmundrotimi <edmundrotimi@gmail.com>
2024-09-18 20:13:24 +0200

    login serializer

---
+++ core/api/auth_login.py
@@ -0,0 +1,7 @@
+# serializers.py
+from rest_framework import serializers
+
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField()
