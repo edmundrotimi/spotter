fa99209a0abf8202fb38b85d7aa8e405aa85ebeb
edmundrotimi <edmundrotimi@gmail.com>
2024-09-19 10:26:03 +0200

    allow user of bearer token in Swagger API

--- core/project/settings/api.py
+++ core/project/settings/api.py
@@ -8,7 +8,18 @@ REST_FRAMEWORK = {
 }
 
 Simple_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=35),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ALGORITHM': 'HS512',
+}
+
+SWAGGER_SETTINGS = {
+    'USE_SESSION_AUTH': False,
+    'SECURITY_DEFINITIONS': {
+        'Bearer': {
+            'type': 'apiKey',
+            'name': 'Authorization',
+            'in': 'header'
+        }
+    }
 }
