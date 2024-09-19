50e411e26e1148485e2ef3c1e8f5c46af548092c
edmundrotimi <edmundrotimi@gmail.com>
2024-09-19 10:27:04 +0200

    add recommendation system

--- core/api/utils/recommendations.py
+++ core/api/utils/recommendations.py
@@ -4,12 +4,7 @@ from core.api.utils.similarity import calculate_similarity
 from core.books.models import Book, BookFav
 
 
-def user_recommend_fav_books(rec_num=5):
-    # Get all the books rated by the user
-    favs = BookFav.objects.order_by('-fav_count')
-
-    if not favs.exists():
-        return []
+def user_recommend_fav_books(rec_num=5, title=''):
 
     # Calculate similarity matrix for books
     similarity_matrix, titles = calculate_similarity()
@@ -18,20 +13,20 @@ def user_recommend_fav_books(rec_num=5):
     recommended_books = set()
 
     # Loop through the books  has rated highly
-    for fav_rating in favs:
-        book_favs = titles.index(fav_rating.book.title)
-        similar_books_indices = np.argsort(similarity_matrix[book_favs])[::-1]
 
-        # Get top_n similar books
-        for idx in similar_books_indices[1:rec_num + 1]:  # Skip the first one (itself)
-            similar_book_title = titles[idx]
-            similar_book = Book.objects.get(title=similar_book_title)
+    book_favs = titles.index(title)
+    similar_books_indices = np.argsort(similarity_matrix[book_favs])[::-1]
 
-            # Avoid recommending books the user has already rated
-            if not BookFav.objects.filter(book=similar_book).exists():
-                recommended_books.add(similar_book)
+    # Get similar books
+    for idx in similar_books_indices[1:rec_num + 1]:  # Skip the first one (itself)
+        similar_book_title = titles[idx]
+        similar_book = Book.objects.get(title=similar_book_title)
 
-            if len(recommended_books) >= rec_num:
-                break
+        # Avoid recommending books the user has already rated
+        if not BookFav.objects.filter(book=similar_book).exists():
+            recommended_books.add(similar_book)
+
+        if len(recommended_books) >= rec_num:
+            break
 
     return list(recommended_books)
